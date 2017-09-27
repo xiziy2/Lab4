dc19286cf0f4ebb3fa681de3c033f9189bd30b1d
Xizi Yang <Xizi Yang@LAPTOP-FDG3E9I8>
2017-09-27 16:23:18 -0500

    LAB4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -11,16 +11,19 @@
 public class ReclamationProject
 {
     static String doit(String a,String b){
-        if (a.length() > b.length()){
+        if (a.length() > b.length()) {
             String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
+            a = b;
+            b = c;
+            }
+        String r = "";
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
                 for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+                    if (a.regionMatches(i, b, k, j) && j >r.length()) {
+                    r = a.substring(i,i + j);
+                    }
+                }
+            }
+        } return r;
 }
